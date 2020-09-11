c3ca30e8d5345b06439d4aa243cc3241d2ab6b1e
Dorian Taylor <code@doriantaylor.com>
2020-09-11 06:22:24 -0700

    teeing up a refactor

--- lib/graph/implementation.rb
+++ lib/graph/implementation.rb
@@ -18,6 +18,34 @@ class Graph::Implementation
 
   public
 
+  # Convert a RGL::DirectedAdjacencyGraph into this graph type.
+  #
+  # @param graph [RGL::DirectedAdjacencyGraph] an input graph
+  # @return [Graph::Implementation] a new graph
+  #
+  def self.from_rgl graph
+    raise ArgumentError,
+      'graph must look like an RGL::DirectedAdjacencyGraph' unless
+      %i[directed? each_vertex each_adjacent].all? { |m| graph.respond_to? m }
+    nodes = []
+    edges = {}
+
+    graph.each_vertex do |s|
+      # extract nodes indiscriminately
+      nodes << s
+      # now extract edges
+      graph.each_adjacent(s) { |o| (edges[s] ||= []) << o }
+    end
+
+    # now initialize
+    self.new nodes: nodes, edges: edges
+  end
+
+  # Initialize the object.
+  #
+  # @param nodes [Array] An array of (possibly disconnected) nodes.
+  # @param edges [Hash] A hash relating nodes to an array-like set of edges.
+  #
   def initialize nodes: [], edges: {}
     @nodes = {} # node metadata (just degrees for now)
     @fwd   = {} # forward edges
@@ -30,31 +58,77 @@ class Graph::Implementation
     end
   end
 
-  def nodes
-    @nodes.keys
+  # Return the nodes in the graph.
+  #
+  # @yield [node] Iterate over the nodes.
+  # @yieldparam node [Object] The node passed to the block.
+  # @return [Array] The nodes.
+  #
+  def nodes &block
+    block ? @nodes.each { |n, _| block.call n } : @nodes.keys
   end
 
+  # Return the set of edges in the graph.
+  #
+  # @yield [source, target] The source-target pair.
+  # @yieldparam source [Object] The source node.
+  # @yieldparam target [Object] The target node.
+  # @return [Array] The edge pairs.
+  #
+  def edges &block
+    if block
+      @fwd.each { |s, tgt| tgt.each { |t| block.call s, t } }
+    else
+      @fwd.map { |s, tgt| tgt.map { |t| [s, t] } }.flatten 1
+    end
+  end
+
+  # Return the in-degree of the given node.
+  #
+  # @param node [Object] The node in question.
+  # @return [Integer,nil] The in-degree, or nil if the node does not exist.
+  #
   def indegree node
     return unless @nodes[node]
     @nodes[node][:indegree]
   end
 
+  # Return the out-degree of the given node.
+  #
+  # @param node [Object] The node in question.
+  # @return [Integer,nil] The out-degree, or nil if the node does not exist.
+  #
   def outdegree node
     return unless @nodes[node]
     @nodes[node][:outdegree]
   end
 
+  # Return the degree (in-degree + out-degree) of the given node.
+  #
+  # @param node [Object] The node in question.
+  # @return [Integer,nil] The degree, or nil if the node does not exist.
+  #
   def degree node
     return unless @nodes[node]
     @nodes[node][:outdegree] + @nodes[node][:indegree]
   end
 
+  # Add a node to the graph.
+  #
+  # @param node [Object] The node to add.
+  # @return [Object]
+  #
   def add node
     @nodes[node] ||= { indegree: 0, outdegree: 0 }
 
     node
   end
 
+  # Remove a node (and adjacent edges) from the graph.
+  #
+  # @param node [Object] The node to remove.
+  # @return [Object]
+  #
   def remove node
     [[@rev, @fwd, :outdegree], [@fwd, @rev, :indegree]].each do |a, b, k|
       if a[node]
@@ -72,35 +146,56 @@ class Graph::Implementation
     node
   end
 
-  def add_edge s, t
-    @fwd[s] ||= Set.new
-    @rev[t] ||= Set.new
+  # Add an edge to the graph.
+  #
+  # @param source [Object] The source node
+  # @param target [Object] The target node
+  # @return [Array,nil] The source-target pair or nil
+  #  if the edge is already there.
+  #
+  def add_edge source, target
+    @fwd[source] ||= Set.new
+    @rev[target] ||= Set.new
 
-    [s, t].each { |x| add x }
+    [source, target].each { |x| add x }
 
     # assume this is symmetric, that @rev[t] also does not include s
-    return if @fwd[s].include? t
+    return if @fwd[source].include? target
 
-    @fwd[s] << t
-    @rev[t] << s
-    @nodes[s][:outdegree] += 1
-    @nodes[t][:indegree]  += 1
+    @fwd[source] << target
+    @rev[target] << source
+    @nodes[source][:outdegree] += 1
+    @nodes[target][:indegree]  += 1
 
-    [s, t]
+    [source, target]
   end
 
-  def remove_edge s, t
-    return unless @fwd[s].include? t
+  # Remove an edge from the graph.
+  #
+  # @param source [Object] The source node
+  # @param target [Object] The target node
+  # @return [Array,nil] The source-target pair or nil
+  #  if the edge is not present.
+  #
+  def remove_edge source, target
+    return unless @fwd[source].include? target
 
-    @fwd[s].delete t
-    @rev[t].delete s
-    @nodes[s][:outdegree] -= 1
-    @nodes[t][:indegree]  -= 1
+    @fwd[source].delete target
+    @rev[target].delete source
+    @nodes[source][:outdegree] -= 1
+    @nodes[target][:indegree]  -= 1
 
-    [s, t]
+    [source, target]
   end
 
-  def invert s, t
-    add_edge t, s if remove_edge s, t
+  # Invert an edge.
+  #
+  # @param source [Object] The source node
+  # @param target [Object] The target node
+  # @return [Array,nil] The inverted source-target pair or nil
+  #  if the original edge is not present.
+  #
+  def invert source, target
+    add_edge target, source if remove_edge source, target
   end
 end
--- lib/graph/sugiyama.rb
+++ lib/graph/sugiyama.rb
@@ -1,4 +1,5 @@
-require "graph/sugiyama/version"
+require 'graph/sugiyama/version'
+require 'graph/implementation'
 
 require 'set'
 
@@ -66,95 +67,98 @@ module Graph::Sugiyama
 
   public
 
-  # Returns a modified graph and the set of edges that were reversed.
-  def remove_cycles graph: nil, nodes: {}, edges: {}
-    # this 
-    nodes, fwd, rev = if graph and graph.respond_to? :each_vertex
-                        prepare_rgl graph
-                      else
-                        { nodes: nodes, fwd: edges, rev: make_rev(edges) } 
-                      end.values_at(:nodes, :fwd, :rev)
+  class << self
 
-    # this is something we keep
-    edges = fwd.dup
+    # Returns a modified graph and the set of edges that were reversed.
+    def remove_cycles graph # graph: nil, nodes: {}, edges: {}
+      # this 
+      nodes, fwd, rev = if graph and graph.respond_to? :each_vertex
+                          prepare_rgl graph
+                        else
+                          { nodes: nodes, fwd: edges, rev: make_rev(edges) }
+                        end.values_at(:nodes, :fwd, :rev)
 
-    # this is something we destroy
-    n = nodes.dup
+      # this is something we keep
+      edges = fwd.dup
 
-    # what follows is algorithm 13.3 (greedy cycle removal) from
-    # healy/nikolov graph drawing handbook chapter 13 on hierarchical
-    # drawing algorithms.
+      # this is something we destroy
+      n = nodes.dup
 
-    # left and right node sequences
-    seql = []
-    seqr = []
+      # what follows is algorithm 13.3 (greedy cycle removal) from
+      # healy/nikolov graph drawing handbook chapter 13 on hierarchical
+      # drawing algorithms.
 
-    until n.empty? do
-      # detect sinks
-      while v = n.keys.detect { |x| n[x][:outdegree] == 0 }
-        remove v, n, fwd, rev
-        seqr.unshift v # prepend to sink the right side
+      # left and right node sequences
+      seql = []
+      seqr = []
+
+      until n.empty? do
+        # detect sinks
+        while v = n.keys.detect { |x| n[x][:outdegree] == 0 }
+          remove v, n, fwd, rev
+          seqr.unshift v # prepend to sink the right side
+        end
+        # detect sources
+        while u = n.keys.detect { |x| n[x][:indegree] == 0 }
+          remove u, n, fwd, rev
+          seql.push u # append the source to the left side
+        end
+
+        unless n.empty?
+          w = n.keys.sort do |a, b|
+            # outdegree minus indegree gives us
+            x = (n[b][:outdegree] - n[b][:indegree]) <=>
+              (n[a][:outdegree] - n[a][:indegree])
+            # sort by node lexical representation if tied so we always
+            # get the same result
+            x == 0 ? a <=> b : x
+          end.first
+
+          remove w, n, fwd, rev
+
+          seql.push w
+        end
       end
-      # detect sources
-      while u = n.keys.detect { |x| n[x][:indegree] == 0 }
-        remove u, n, fwd, rev
-        seql.push u # append the source to the left side
-      end
 
-      unless n.empty?
-        w = n.keys.sort do |a, b|
-          # outdegree minus indegree gives us 
-          x = (n[b][:outdegree] - n[b][:indegree]) <=>
-            (n[a][:outdegree] - n[a][:indegree])
-          # sort by node lexical representation if tied so we always
-          # get the same result
-          x == 0 ? a <=> b : x
-        end.first
+      # now any edges going from higher up the sequence to lower
+      # constitute a feedback arc set. we make this into a hash so we
+      # can look it up.
+      seq = {}
+      (seql + seqr).each_with_index { |x, i| seq[x] = i }
 
-        remove w, n, fwd, rev
+      fas = edges.map do |s, tgt|
+        # keep the leftward-pointing edges (source has higher seq number)
+        [s, tgt.dup.keep_if { |t| seq[s] > seq[t] }]
+      end.reject { |x| x.last.empty? }.to_h
 
-        seql.push w
-      end
+      { nodes: nodes, edges: edges, fas: fas }
     end
 
-    # now any edges going from higher up the sequence to lower
-    # constitute a feedback arc set. we make this into a hash so we
-    # can look it up.
-    seq = {}
-    (seql + seqr).each_with_index { |x, i| seq[x] = i }
+    # Returns a modified graph (with dummy nodes) and an array of layer
+    # assignments.
+    def assign_layers graph, concentrate: false
 
-    fas = edges.map do |s, tgt|
-      # keep the leftward-pointing edges (source has higher seq number)
-      [s, tgt.dup.keep_if { |t| seq[s] > seq[t] }]
-    end.reject { |x| x.last.empty? }.to_h
+      # [graph, layers]
+    end
 
-    { nodes: nodes, edges: edges, fas: fas }
-  end
+    # Returns a modified graph (again with EC dummy nodes) and a new
+    # array of layer assignments.
+    def concentrate_edges graph, layers
 
-  # Returns a modified graph (with dummy nodes) and an array of layer
-  # assignments.
-  def assign_layers graph, concentrate: false
+      # [graph, layers]
+    end
 
-    # [graph, layers]
-  end
+    # Returns the layer assignment with the correct ordering.
+    def reduce_crossings graph, layers, tighten: false
 
-  # Returns a modified graph (again with EC dummy nodes) and a new
-  # array of layer assignments.
-  def concentrate_edges graph, layers
+      # layers
+    end
 
-    # [graph, layers]
-  end
-
-  # Returns the layer assignment with the correct ordering.
-  def reduce_crossings graph, layers, tighten: false
-
-    # layers
-  end
-
-  # Tightens edges by aligning nodes longitudinally. Returns a layer
-  # assignment the modified ordering, with columns possibly padded with `nil`.
-  def tighten_edges graph, layers
+    # Tightens edges by aligning nodes longitudinally. Returns a layer
+    # assignment the modified ordering, with columns possibly padded with `nil`.
+    def tighten_edges graph, layers
     
-    # layers
+      # layers
+    end
   end
 end
